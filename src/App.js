7656146972d88a6c29667cda8573409ce37f7a03
romanet alex <ag.romanet96@gmail.com>
2021-03-28 19:11:51 +0300

    added drills page, update menu items with withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 
-function App() {
+const DrillsPage = () => (
+  <div>
+    <h1>DRILLS</h1>
+  </div>
+);
+
+function App() { 
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/drills' component={DrillsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component'
+import './directory.styles.scss';
 
 class Directory extends React.Component {
     constructor() {
@@ -7,37 +8,37 @@ class Directory extends React.Component {
 
         this.state = {
             sections: [
-                {
+                  {
                     title: 'drills',
                     imageUrl: 'https://www.discounttrader.com.au/wp-content/uploads/2019/02/SB-18-LTX-BL-I-602352650-CORDLESS-HAMMER-DRILL-LIHD-4.0-1.png',
                     id: 1,
-                    linkUrl: 'shop/drills'
+                    linkUrl: 'drills'
                   },
                   {
                     title: 'saws',
                     imageUrl: 'https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_1.0,f_auto,h_843,q_auto,w_1500/c_pad,h_843,w_1500/R8129938-01?pgw=1&pgwact=1',
                     id: 2,
-                    linkUrl: 'shop/saws'
+                    linkUrl: 'saws'
                   },
                   {
                     title: 'grinders',
                     imageUrl: 'https://www.dinamitek.com/620-large_default/gws-9-125-angle-grinder-bosch.jpg',
                     id: 3,
-                    linkUrl: 'shop/grinders'
+                    linkUrl: 'grinders'
                   },
                   {
                     title: 'corded',
                     imageUrl: 'https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_1.0,f_auto,h_843,q_auto,w_1500/c_pad,h_843,w_1500/R8692212-01?pgw=1&pgwact=1',
                     size: 'large',
                     id: 4,
-                    linkUrl: 'shop/corded'
+                    linkUrl: 'corded'
                   },
                   {
                     title: 'cordless',
                     imageUrl: 'https://image.made-in-china.com/2f0j00aLUfzmTMIWkg/20V-Lithium-Battery-Power-Tool-Cordless-Rotary-Hammer-Drill-8202.jpg' ,
                     size: 'large',
                     id: 5,
-                    linkUrl: 'shop/cordless'
+                    linkUrl: 'cordless'
                   }
             ]
         }
@@ -47,8 +48,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
             {
-                this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                this.state.sections.map(({id, ...otherSectionProps}) => (
+                    <MenuItem key={id} {...otherSectionProps}/>
                 ))
             }
             </div>
